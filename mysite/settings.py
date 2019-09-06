0d0d5c8677d36f007df873de45713b1d46617107
olegsergeyev <flpok1@gmail.com>
2019-09-06 16:43:25 +0300

    db change

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_db',
-        'USER' : 'oleg',
-        'PASSWORD' : '8EDc0SDd',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
